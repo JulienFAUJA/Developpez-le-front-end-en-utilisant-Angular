637cfad626bbdc4f7a0a0cc0aa2e6fc035c4e8b1
JulienFAUJA <julienfaujanet@gmail.com>
2024-03-14 14:21:39 +0100

    début page not found 2

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-not-found',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
+  phrase!: string;
+  @Input() elem_id!:number;
+  public not_found_phrase$!: Observable<string>;
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.phrase= "erreur avec l'id: "+this.elem_id;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['elem_id']) {
+      this.phrase= "erreur avec l'id: "+this.elem_id;
+    }
   }
 
 }
